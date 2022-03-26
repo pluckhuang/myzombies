ce96b9d8a05114093d2f0cd477cc8ae560cea166
huangjiangbo <huangjiangbo.z@gmail.com>
2022-03-26 14:15:09 +0800

    add Zombiefactory testcase

--- contracts/Zombiefactory.sol
+++ contracts/Zombiefactory.sol
@@ -44,7 +44,10 @@ contract ZombieFactory is Ownable {
     }
 
     function createRandomZombie(string memory _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
+        require(
+            ownerZombieCount[msg.sender] == 0,
+            "everyone just can create a zombie."
+        );
         uint256 randDna = _generateRandomDna(_name);
         randDna = randDna - (randDna % 100);
         _createZombie(_name, randDna);
--- test/zombiefactory.js
+++ test/zombiefactory.js
@@ -1,16 +1,20 @@
 const zombiefactory = artifacts.require("Zombiefactory");
+const utils = require("./helpers/utils");
+const zombieNames = ["Zombie 1", "Zombie 2"];
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("zombiefactory", function (accounts) {
-  it("should create zombie", async () => {
-    const zombiefactoryInstance = await zombiefactory.deployed();
-    await zombiefactoryInstance.createRandomZombie("pluck");
-    assert.equal(await zombiefactoryInstance.checkZombieToOwner(0), accounts[0], "cannot create a zombie");
+contract("Zombiefactory", function (accounts) {
+    let [alice, bob] = accounts;
+    let zombiefactoryInstance;
+    beforeEach(async () => {
+        zombiefactoryInstance = await zombiefactory.new();
+    });
+  it("everyone should create zombie", async () => {
+    const result = await zombiefactoryInstance.createRandomZombie(zombieNames[0], {from: alice});
+    assert.equal(result.receipt.status, true);
+    assert.equal(result.logs[0].args.name,zombieNames[0]);
   });
-
-
+  it("everyone should only create a zombie", async () => {
+    await zombiefactoryInstance.createRandomZombie(zombieNames[0], {from: alice});
+    await utils.shouldThrow(zombiefactoryInstance.createRandomZombie(zombieNames[1], {from: alice}));
+  });
 });
